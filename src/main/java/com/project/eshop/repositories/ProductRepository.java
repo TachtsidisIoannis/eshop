be90959d5cacf87cb3b8c8fcac60263b2ea1be09
Giannis <diaforpaixnidia@gmail.com>
2026-03-12 16:15:49 +0200

    Extra stuff

--- src/main/java/com/project/eshop/repositories/ProductRepository.java
+++ src/main/java/com/project/eshop/repositories/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.project.eshop.repositories;
 
 import com.project.eshop.entity.Product;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+	List<Product> findByCategory_Id(Integer categoryId);
 }
