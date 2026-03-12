fe8eee2560b22b39df16d1942b2c483e5cfc50e4
Giannis <diaforpaixnidia@gmail.com>
2026-03-12 16:08:16 +0200

    Another Repo fix

--- src/main/java/com/project/eshop/repositories/OrderItemRepository.java
+++ src/main/java/com/project/eshop/repositories/OrderItemRepository.java
@@ -1,10 +1,13 @@
 package com.project.eshop.repositories;
 
 import com.project.eshop.entity.OrderItem;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
-
+	List<OrderItem> findByOrderId(Integer orderId);
 }
--- src/main/java/com/project/eshop/repositories/StoreInventoryRepository.java
+++ src/main/java/com/project/eshop/repositories/StoreInventoryRepository.java
@@ -1,10 +1,12 @@
 package com.project.eshop.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.project.eshop.entity.StoreInventory;
 
 @Repository
 public interface StoreInventoryRepository extends JpaRepository<StoreInventory, Integer> {
-
+	List<StoreInventory> findByStoreId(Integer storeId);
 }
