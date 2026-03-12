f3beb9cecea964ed17c6499fb92924f3fd67516d
Giannis <diaforpaixnidia@gmail.com>
2026-03-12 15:52:14 +0200

    Small EmpRepo fix

--- src/main/java/com/project/eshop/repositories/EmployeeRepository.java
+++ src/main/java/com/project/eshop/repositories/EmployeeRepository.java
@@ -1,10 +1,14 @@
 package com.project.eshop.repositories;
 
 import com.project.eshop.entity.Employee;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
+	
+	List<Employee> findByStoreId(Integer storeId);
 }
